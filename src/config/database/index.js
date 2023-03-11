b1d171c7c09d847e9f539a643375903b48fa8301
Anteel1 <luonglkvn100@gmail.com>
2023-03-12 00:38:03 +0700

    downgrade mongoose and mongodb

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDB = async () => {
   await mongoose
     .connect("mongodb://localhost:27017/demo_mongo")
-    .then(() => console.log("Connected!"))
+    .then(() => console.log("Connected"))
     .catch((error) => console.log(error));
 };
 
